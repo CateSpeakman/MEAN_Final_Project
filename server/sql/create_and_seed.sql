c6704f5f6c0d2b0fd1581a2dbe0d91c9eef9f68a
Cate Speakman <speakmancate@gmail.com>
2019-09-29 20:37:20 -0400

    made create and seed file

---
+++ server/sql/create_and_seed.sql
@@ -0,0 +1,19 @@
+drop database hca;
+create database hca;
+use DEMO;
+
+create table USERS (
+	ID		 	int(10) NOT NULL AUTO_INCREMENT, 
+	username 	varchar(255) NOT NULL, 
+	email 		varchar(255) NOT NULL, 
+    password    varchar(255) NOT NULL,
+	IS_ADMIN	smallint(1) NOT NULL DEFAULT 0,
+	createdAt  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	updatedAt  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	UNIQUE		UQ_USER_1 (USER_NAME),
+	PRIMARY KEY(ID)
+);
+
+INSERT INTO Users (user_name, email, is_admin) VALUES ('Admin', 'admin@test.com', 1);
+
+select * from users;
